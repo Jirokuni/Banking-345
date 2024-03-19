df87050bc3f83e73309341f6faf57f5dd9bb3c28
Jerome Gesmundo <jerome.gesmundo@yahoo.com>
2024-03-19 19:30:50 +0800

    home link

---
+++ src/home.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Home = () => {
+
+return (
+    <div className='dash-box'>
+    <div className='promo-boxes'>
+        <div id='box' className='interest-box'>
+            <h2>High Interest Rates!</h2>
+        </div>
+        <div id='box' className='rebate-box'>
+            <h2>Rebates on our Card!</h2>
+        </div>
+        <div id='box' className='cashback-box'>
+            <h2>Cashbacks!</h2>
+        </div>
+        <div id='box' className='cashback-box'>
+            <h2>Cashbacks!</h2>
+        </div>
+        <div id='box' className='cashback-box'>
+            <h2>Cashbacks!</h2>
+        </div>
+        <div id='box' className='cashback-box'>
+            <h2>Cashbacks!</h2>
+        </div>
+    </div>
+    </div>
+)
+
+}
+
+
+export default Home;
