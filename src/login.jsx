1949ba5decac7588803c7bb275e4c3f446e0b420
Jerome <jerome.gesmundo@yahoo.com>
2024-03-23 04:34:08 +0800

    Design updates 3

--- src/login.jsx
+++ src/login.jsx
@@ -31,17 +31,20 @@ export const Login = (props) => {
 
     return(
         <>
-        <NavBar />
-        <div className="login">
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" />
-            <label htmlFor="password">Password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*******" id="password" />
-            <button type="submit">Login</button>
-        </form>
-        <button onClick={handleCreateAccount}>Don't have an account? Register here.</button>
+        <div>
+            <NavBar />
         </div>
+            <div className="login-container">
+                <div className="login">
+                    <form onSubmit={handleSubmit} className="login-box">
+                        <h1>Log In</h1>
+                        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" />
+                        <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*******" id="password" />
+                        <button type="submit">Login</button>
+                        <button onClick={handleCreateAccount}>Don't have an account? Register here.</button>
+                  </form>
+                 </div>
+            </div>
         </>
     )
 }
